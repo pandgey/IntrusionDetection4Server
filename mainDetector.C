870dd8e3197879881e61ab7cb7362f5ff4b73520
pandgey <krittapong.ti@gmail.com>
2025-06-23 19:39:43 +0700

    fully functional? Not tested yet, it only has icmp detection

--- mainDetector.C
+++ mainDetector.C
@@ -18,10 +18,10 @@ int main() {
     long bytes_to_read;
     int i;
 
-    //open the serial port
+    // open the serial port
     serial_fd = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_SYNC);
 
-    // Configure serial port
+    // configure serial port
     tcgetattr(serial_fd, &tty);
     cfsetospeed(&tty, B9600);
     cfsetisspeed(&tty, B9600);
@@ -37,4 +37,56 @@ int main() {
     tty.c_cflag &= ~CSTOPB;
     tty.c_cflag &= ~CRTSCTS;
     tcsetattr(serial_fd, TCSANOW, &tty);
+
+    sleep(2);
+
+    // get file size
+    stat("/var/log/snort/alerts", &st);
+    last_size = st.st_size;
+
+    write(serial_fd, "A", 1);
+
+    while (1) {
+
+        //get current file size
+        stat("/var/log/snort/alerts", &st);
+        current_size = st.st_size;
+
+        if (current_size > last_size) {
+            //open file and seek to last position
+            file = fopen("/var/log/snort/alerts", "r");
+            fseek(file, last_size, SEEK_SET);
+
+            // read new data
+            bytes_to_read = current_size - last_size;
+            buffer = malloc(bytes_to_read + 1);
+            fread(buffer, 1, bytes_to_read, file);
+            buffer[bytes_to_read] = '\0';
+            fclose(file);
+
+            // convert to lowercase
+            lower_buffer = malloc(bytes_to_read + 1);
+            for (i = 0, i <= bytes_to_read; i++) {
+                lower_buffer[i] = tolower(buffer[i]);
+            }
+
+            // check for alerts
+
+            // check for icmp
+            if (strstr(lower_buffer, "icmp")) {
+                printf("ICMP Alert\n");
+                write(serial_fd, "I", 1);
+            }
+
+            free(buffer);
+            free(lower_buffer);
+            last_size = current_size;
+
+        }
+
+        sleep(1);
+    }
+
+    close(serial_fd);
+    return 0;
 }
